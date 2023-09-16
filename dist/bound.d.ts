eb93e92286f874da20de4bbff43c64d628d49c2c
Ivan Bakaidov <ibakaidov@ya.ru>
2023-09-16 15:08:31 +0300

    remove bins

--- dist/bound.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Bound {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    constructor(x: number, y: number, width: number, height: number);
-    static fromArray(array: number[]): Bound;
-    toString(): string;
-}
